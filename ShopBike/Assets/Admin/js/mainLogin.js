69393ee8e23054880e303fd7111e21482e2e3bfd
Đỗ Tiến Khải <dotienkhai06@gmail.com>
2021-08-05 08:48:25 +0700

    Validate && chỉnh sửa client

---
+++ ShopBike/Assets/Admin/js/mainLogin.js
@@ -0,0 +1,18 @@
+﻿function validateForm() {
+    var email = document.myform.email.value;
+    atpos = email.indexOf("@");
+    dotpos = email.lastIndexOf(".");
+    if (email == "") {
+        alert("Email không được để trống !!");
+        document.myform.email.focus();
+        return false;
+    }
+    if (atpos < 1 || (dotpos - atpos < 2)) {
+        alert("Nhập đúng địng dạng email.");
+        document.myform.email.focus();
+        return false;
+    }
+    return true;
+}
+
+
