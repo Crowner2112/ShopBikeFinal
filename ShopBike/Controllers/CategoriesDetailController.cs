e1d4ce54784164858cba886e7f8797c919a77a32
Do Duc <docongtu1234@gmail.com>
2021-08-07 17:30:57 +0700

    Update more

---
+++ ShopBike/Controllers/CategoriesDetailController.cs
@@ -0,0 +1,20 @@
+﻿using Models.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopBike.Controllers
+{
+    public class CategoriesDetailController : Controller
+    {
+        private ImageDAO dao = new ImageDAO();
+        public ActionResult Index(int id)
+        {
+            var model = dao.GetByCategoryId(id);
+            ViewBag.CategoryName = model.FirstOrDefault().Product.Category.Name.ToLower();
+            return View(model);
+        }
+    }
+}
